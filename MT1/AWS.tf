d569e599a688f57f436b926c020c8c27c609a98c
Prajwal H R <prajwalravikumar10@gmail.com>
2022-03-16 22:31:03 +0530

    enter

--- MT1/AWS.tf
+++ MT1/AWS.tf
@@ -5,17 +5,19 @@ provider "aws" {
 }
 
 # we can change the provider like azure , google cloud
+# If we run it twice it will not create a new or same aws server 
 
 resource "aws_instance" "My_First_Server" {
     ami = "ami-0e472ba40eb589f49"
     instance_type = "t2.micro"
-
-  
+    tags = {
+      name = "S1"
+    }
 }
 # github
 # terraform init
 # for dry run : terraform plan [ just like what if? in ansible ]
 # terraform apply [ another what_IF? + it asky permission for each stage]
 # done
-# terr
+
 
